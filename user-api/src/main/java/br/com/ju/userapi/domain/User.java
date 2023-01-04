118ebe1285e700f315df4280a5cf322fcf1ec1ff
Jusley Souza <jusleysouza31@gmail.com>
2023-01-04 19:19:48 -0300

    Criação e implementação da entidade de Usuario

---
+++ user-api/src/main/java/br/com/ju/userapi/domain/User.java
@@ -0,0 +1,30 @@
+package br.com.ju.userapi.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity(name = "TB-USER")
+public class User {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	private Double hourlyPrice;
+
+}
