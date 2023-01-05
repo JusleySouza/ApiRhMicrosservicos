d455114fcf83cba202769def92bc464f2e462e00
Jusley Souza <jusleysouza31@gmail.com>
2023-01-05 20:17:40 -0300

    Refatoração do código

--- payroll-api/src/main/java/br/com/ju/payrollapi/resources/PayrollResource.java
+++ payroll-api/src/main/java/br/com/ju/payrollapi/resources/PayrollResource.java
@@ -8,19 +8,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ju.payrollapi.domain.Payroll;
+import br.com.ju.payrollapi.domain.User;
+import br.com.ju.payrollapi.feignclients.UserFeign;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/payments")
 public class PayrollResource {
 	
+	private final UserFeign userFeign;
+	
 	@GetMapping(value = "/{workerId}")
 	public ResponseEntity<Payroll> getPayment(@PathVariable Long workerId, @RequestBody Payroll payment){
+		
+		User user = userFeign.findById(workerId).getBody();
+		
 		return ResponseEntity.ok().body(
-				new Payroll("Sarah",
+				new Payroll(
+						user.getName(),
 						payment.getDescription(), 
-						payment.getHourlyPrice(), 
-						100.0,
-						payment.getHourlyPrice() * payment.getWorkedHours())
+						user.getHourlyPrice(), 
+						payment.getWorkedHours(),
+						user.getHourlyPrice() * payment.getWorkedHours())
 		);
 	}
 
