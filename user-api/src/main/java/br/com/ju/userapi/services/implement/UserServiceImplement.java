c81ff57369cc7d961233d7b586835267903d558b
Jusley Souza <jusleysouza31@gmail.com>
2023-01-18 16:14:40 -0300

    Adicionando o logger a classe de implementação do Usuário

--- user-api/src/main/java/br/com/ju/userapi/services/implement/UserServiceImplement.java
+++ user-api/src/main/java/br/com/ju/userapi/services/implement/UserServiceImplement.java
@@ -2,7 +2,7 @@ package br.com.ju.userapi.services.implement;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import br.com.ju.userapi.domain.User;
@@ -10,15 +10,20 @@ import br.com.ju.userapi.repositories.UserRepository;
 import br.com.ju.userapi.services.UserService;
 import br.com.ju.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImplement implements UserService{
+	
+	private final Environment env;
 	
 	private final UserRepository repository;
 
 	@Override
 	public User findById(Long id) {
+		log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + "port");
 		return repository.findById(id).orElseThrow(() -> 
 		new ObjectNotFoundException("Object not found!!"));
 	}
